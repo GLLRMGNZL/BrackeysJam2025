7ed9f073b5870fb79073d5a468fea723f02b6107
GLLRMGNZL <guillegonza95@gmail.com>
2025-02-17 13:05:08 +0100

    Add mouse selection on objects

---
+++ NothingCanGoWrong/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseClick : MonoBehaviour
+{
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    private void Update()
+    {
+        CheckSelectionOnClick();
+    }
+
+    private void CheckSelectionOnClick()
+    {
+        if (highlight != null && highlight != selection)
+        {
+            highlight.gameObject.GetComponent<Outline>().enabled = false;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit))
+        {
+            highlight = raycastHit.transform;
+            if (highlight.CompareTag("Selectable") && highlight != selection)
+            {
+                if (highlight.gameObject.GetComponent<Outline>() != null)
+                {
+                    highlight.gameObject.GetComponent<Outline>().enabled = true;
+                }
+                else
+                {
+                    Outline outline = highlight.gameObject.AddComponent<Outline>();
+                    outline.enabled = true;
+                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.white;
+                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7f;
+                }
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
+
+        // Mouse Selection
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (highlight)
+            {
+
+                if (selection != null)
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = false;
+                }
+                selection = raycastHit.transform;
+                selection.gameObject.GetComponent<Outline>().enabled = true;
+
+                highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.blue;
+                highlight.gameObject.GetComponent<Outline>().OutlineWidth = 20f;
+
+                Debug.Log("Objeto seleccionado: " + selection.gameObject.name);
+            }
+            else
+            {
+                if (selection != null && !EventSystem.current.IsPointerOverGameObject())
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = false;
+                    selection = null;
+                }
+            }
+        }
+    }
+}
--- NothingCanGoWrong/Assets/Scripts/StarSystem.cs
+++ NothingCanGoWrong/Assets/Scripts/StarSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class StarSystem : MonoBehaviour
@@ -10,7 +11,7 @@ public class StarSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        attractors = GameObject.FindGameObjectsWithTag("Attractor");
+        attractors = FindObjectsOfType<Attractor>().Select(a => a.gameObject).ToArray();
         InitialVelocity();
     }
 
