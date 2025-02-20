f8882af0fa7bcf5fc944d3ecb554ea42fbdd14ba
GLLRMGNZL <guillegonza95@gmail.com>
2025-02-20 15:47:05 +0100

    Add level progress slider + bug fix not showing correct level number on UI

--- NothingCanGoWrong/Assets/Scripts/PlayerStatsUI.cs
+++ NothingCanGoWrong/Assets/Scripts/PlayerStatsUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatsUI : MonoBehaviour
 {
@@ -28,12 +29,14 @@ public class PlayerStatsUI : MonoBehaviour
     public TextMeshProUGUI buildingResources;
     public TextMeshProUGUI travelResources;
     public TextMeshProUGUI techLevel;
+    public Slider techLevelSlider;
 
     private void Update()
     {
         yearText.text = PlayerStats.instance.year.ToString();
         buildingResources.text = PlayerStats.instance.buildingResources.ToString();
         travelResources.text = PlayerStats.instance.travelResources.ToString();
-        techLevel.text = Mathf.Round(PlayerStats.instance.technologyLevel).ToString();
+        techLevel.text = ((int)PlayerStats.instance.technologyLevel).ToString();
+        techLevelSlider.value = PlayerStats.instance.technologyLevel - Mathf.Floor(PlayerStats.instance.technologyLevel);
     }
 }
